fe6a9a984e609528378b63398de8cbd824f17040
Stanley Louis <stanlouis@msn.com>
2019-07-03 15:00:33 -0400

    Creating and display notes

--- server.js
+++ server.js
@@ -14,10 +14,29 @@ app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(`${__dirname}\public`));
+app.use(express.static(`${__dirname}/public`));
 
 app.get('/', (req, res) => {
-  res.send('Hello Express template');
+  fetch('http://localhost:3004/messages').then(response => {
+    response.json().then(data => {
+      res.render('home', { notes: data });
+    });
+  });
+});
+
+app.get('/add_note', (req, res) => {
+  res.render('add_note');
+});
+
+app.post('/add_note', (req, res) => {
+  fetch('http://localhost:3004/messages', {
+    method: 'POST',
+    body: JSON.stringify(req.body),
+    headers: { 'Content-Type': 'application/json' },
+  }).then(response => {
+    res.status(200).send();
+  });
+  res.redirect('/');
 });
 
 const PORT = process.env.PORT || 3000;
